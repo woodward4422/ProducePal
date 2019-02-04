d861ea9df4ae2b42a7b0c61394989972592f07fe
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-02-03 16:51:08 -0800

    [add] Core Location Services

--- Produce Pal/Produce Pal/Controller/MapViewController.swift
+++ Produce Pal/Produce Pal/Controller/MapViewController.swift
@@ -1,1 +1,72 @@
+//
+//  ViewController.swift
+//  Produce Pal
+//
+//  Created by Noah Woodward on 1/29/19.
+//  Copyright © 2019 Noah Woodward. All rights reserved.
+//
 
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+    
+    var locationManager = CLLocationManager()
+    
+    
+    var marketMap: MKMapView {
+        let mapView = MKMapView()
+        mapView.mapType = MKMapType.standard
+        mapView.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        mapView.setUserTrackingMode(.follow, animated: true)
+        mapView.center = view.center
+        return mapView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setupMap()
+        showLocation()
+    }
+    
+    private func setupMap() {
+        let mapView = marketMap
+        view.addSubview(mapView)
+        
+        let leftMargin:CGFloat = 0
+        let topMargin:CGFloat = 0
+        let mapWidth:CGFloat = view.frame.size.width
+        let mapHeight:CGFloat = view.frame.size.height
+        
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        
+    }
+    
+    private func showLocation() {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Check for Location Services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        //Zoom to user location
+        if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 500, longitudinalMeters: 500)
+            marketMap.setRegion(viewRegion, animated: true)
+        }
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+}
